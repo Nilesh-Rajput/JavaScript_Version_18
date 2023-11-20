8ead9c8aa485af7be216f9b8f0785e8ff0935b41
Nilesh-Rajput <120545385+Nilesh-Rajput@users.noreply.github.com>
2023-11-20 13:31:06 +0000

    work on variable like const var and let keywords

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1234567
+let accountName = "Mayur Rajput"
+var accountAmmount = "1234"
+
+
+// accountId = 2 // Not allowed // beacause const are not change onces it declared 
+
+// we can change the let keyword value but not for const
+
+// Preferable to use let instead of var beacuse of the scope of the variable and functional scope of the variable
+
+
+console.log(accountId);
+console.table([accountId,accountAmmount,accountName]);
